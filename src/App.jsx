db0e5ad74d5e556767891648159ea15e4f859a20
Mary Gouge <Marygouge36@gmail.com>
2023-12-04 10:08:00 -0800

    feat: added circleLoader and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import Header from "./Components/Header/";
 import Slider from "./Components/Slider";
 import ProductionHouse from "./Components/ProductionHouse";
 import GenreMovieList from "./Components/GenreMovieList";
+import CircleLoader from "react-spinners/CircleLoader";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="">
       <Header />
       <Slider />
       <ProductionHouse />
-      <GenreMovieList />
+      {loading ? (
+        <CircleLoader size={150} color={"#0BC2DA"} loading={loading} cssOverride={{margin: "5rem auto"}} />
+      ) : (
+        <GenreMovieList />
+      )}
     </div>
   );
 };
